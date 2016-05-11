10221180c21c04056a5d43b43fd1d41e94db5ee9
b1eberg0n <biebergong@gmail.com>
2016-05-11 21:09:45 +0800

    完善中

--- accelerator.py
+++ accelerator.py
@@ -1,37 +1,13 @@
 from flask import Flask, request
 from gevent.wsgi import WSGIServer
 import re
-# import requests
-import socket
-import ssl
-from gevent import monkey
-monkey.patch_socket()
-# monkey.patch_ssl()
+from gevent import socket, ssl
+import time
 
 app = Flask(__name__)
 
 path_p = re.compile('^.+? (.+?) ')
 host_p = re.compile('Host: (.+?)\r')
-# def get_headers(req):
-# 	# print(req)
-# 	headers_raw = req.split('\r\n\r\n')[0].split('\r\n')
-# 	method = headers_raw[0].split(' ')[0]
-# 	headers = { i.split(': ')[0]:i.split(': ')[1]
-# 				for i in headers_raw[2:]
-# 				if i != ''# and 'GET' not in i and 'Host' not in i
-# 				# and 'POST' not in i and 'CONNECT' not in i
-# 				}
-# 	url = host_p.findall(req)[0] + path_p.findall(req)[0]
-# 	if method == 'POST':
-# 		data = req.split('\r\n\r\n')[1].split('&')
-# 		data = { i.split('=')[0]:i.split('=')[1] for i in data }
-# 		print(url, headers, data)
-# 		return method, url, headers, data
-# 	else:
-# 		print(method, url)
-# 		return method, url, headers, ''
-
-
 def get_host_port(req, https=False):
 	host = host_p.findall(req)[0]
 	if ':' in host:
@@ -43,33 +19,13 @@ def get_host_port(req, https=False):
 		return host, port
 
 
-# def get_resp_with_ssl(req):
-# 	print(req)
-# 	host, port = get_host_port(req)
-# 	# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 	s = ssl.wrap_socket(socket.socket())
-# 	s.connect((host, port))
-# 	print('ssl contfinish')
-# 	s.sendall(req.encode('utf-8'))
-	
-# 	resp = b''
-# 	buf = 1
-# 	while buf:
-# 		buf = s.recv(1024*1024*8)
-# 		resp += buf
-# 	return resp
-
-
 def get_resp(req, https=False):
-	# req = request.form['req']
-	# print(req.split('\r\n')[0])
 	url = '{host}{path}'.format(
 		host = req.split('\r\n')[1].split(' ')[1],
 		path = req.split('\r\n')[0].split(' ')[1])
-		# req.split('\r\n')[1].split(' ')[1]
 	info = '{method} {url}'.format(
 		method = req.split(' ')[0], url = url)
-	print(info)
+	# print(info)
 	host, port = get_host_port(req, https)
 	if https:
 		s = ssl.wrap_socket(socket.socket(),
@@ -77,18 +33,16 @@ def get_resp(req, https=False):
 		s.settimeout(5)
 	else:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# print(host, port, req)
 	try:
 		s.connect((host, port))
-		print('connect ok')
+		# print('connect ok')
 	except socket.timeout as e:
 			print(e)
 			return
 	s.sendall(req.encode('utf-8'))
-	print('send ok')
+	# print('send ok')
 	
 	resp = b''
-	# buf = b'1'
 	while 1:
 		# buf and not buf.startswith(b'WebSocket')\
 		  # and not buf.endswith(b'\r\n\r\n'):
@@ -98,67 +52,30 @@ def get_resp(req, https=False):
 			print(e)
 			break
 		
-		print('{}......{}'.format(buf[:50],buf[-50:]))
+		# print('{}......{}'.format(buf[:50],buf[-50:]))
 		resp += buf
 		if not buf or\
 		   buf.startswith(b'WebSocket') and buf.endswith(b'\r\n\r\n'):
 			break
-	print('RECV {}'.format(url))
-	return resp
+	# print('RECV {}'.format(url))
+	return resp, info
 
 
-# s = requests.session()
-# resp = [ 'Server', 'Date', 'Content-Type', 'Vary', 'Expires',
-# 		'cache-control' ]
 @app.route('/', methods=['GET', 'POST'])
 def accelerator():
 	if request.method == 'GET':
 		return 'Railgun.'
 	elif request.method == 'POST':
 		if request.form.get('ssl'):
-			print('ssl')
-			# print(request.form['req'])
-			return get_resp(request.form['req'], https=True)
+			# print('ssl')
+			resp, info= get_resp(request.form['req'], https=True)
 		else:
-			return get_resp(request.form['req'])
-	# elif request.method == 'CONNECT':
-		# return
+			resp, info= get_resp(request.form['req'])
 
-		
-		method, url, headers, data = get_headers(request.form['req'])
-		if method == 'GET':
-			r = s.get('http://{}'.format(url))
-		elif method == 'POST':
-			r = s.post('http://{}'.format(url), data=data)
-			# print(r.text)
-		ok = 'OK' if r.ok else ''
-		if r.headers.get('content-length'):
-			del r.headers['content-length']
-		# del r.headers['expires']
-		# del r.headers['x-cache']
-		# del r.headers['x-ec-custom-error']
-		# del r.headers['vary']
-		# del r.headers['last-modified']
-		# del r.headers['etag']
-		# del r.headers['cache-control']
-		if r.headers.get('transfer-encoding'):
-			del r.headers['transfer-encoding']
-		# del r.headers['date']
-		# del r.headers['content-type']
-		if r.headers.get('content-encoding'):
-			del r.headers['content-encoding']
-		headers = [ '{}: {}'.format(i,r.headers[i]) for i in r.headers ]
-		# headers = ['{}: {}'.format(
-		# 	'content-length',r.headers['content-length']),
-		# 	'{}: {}'.format('expires',r.headers['expires'])
-		# 	]
-		headers = '\r\n'.join(headers) + '\r\n\r\n'
-		res = 'HTTP/1.1 {code} {ok}\r\n{headers}'.format(code=r.status_code,ok=ok,headers=headers)
-		# res = 'HTTP/1.1 {code} {ok}\r\n\r\n'.format(code=r.status_code,ok=ok)
-		res = res.encode('utf-8') + r.content
-		# print(res)
-		return res
-		# return r.content
+		print(request.remote_addr,
+			  '- - [{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
+			  '"{}"'.format(info) )
+		return resp
 
 
 http_server = WSGIServer(('', 8080), app)
--- railgun.py
+++ railgun.py
@@ -1,31 +1,42 @@
 import socket
 from urllib.parse import urlparse#, urlunparse
 from threading import Thread
-from multiprocessing import Process
+# from multiprocessing import Process
 import re
 import requests
 import ssl
 from certutil import CertUtil
+import time
+# import queue
 
 def parse_header(raw_headers):
 	request_lines = raw_headers.split('\r\n')
 	first_line = request_lines[0].split(' ')
 	method = first_line[0]
-	full_path = first_line[1]
-	version = first_line[2]
-	print("%s %s" % (method, full_path))
+	if len(first_line) >= 2: 
+		full_path = first_line[1]
+	else:
+		full_path = first_line[0]
+	try:
+		host = request_lines[1].split(' ')[1]
+	except IndexError:
+		# print(raw_headers)
+		host = ''
+	# print(host)
+	# version = first_line[2]
+	# print("%s %s" % (method, full_path))
 	(scm, netloc, path, params, query, fragment) \
 		= urlparse(full_path, 'http')
-	if method == 'CONNECT':
-		address = (path.split(':')[0], int(path.split(':')[1]))
-	else:
-		# 如果url中有‘：’就指定端口，没有则为默认80端口
-		i = netloc.find(':')
-		if i >= 0:
-			address = netloc[:i], int(netloc[i + 1:])
-		else:
-			address = netloc, 80
-	return method, version, scm, address, path, params, query, fragment
+	# if method == 'CONNECT':
+	# 	address = (path.split(':')[0], int(path.split(':')[1]))
+	# else:
+	# 	# 如果url中有‘：’就指定端口，没有则为默认80端口
+	# 	i = netloc.find(':')
+	# 	if i >= 0:
+	# 		address = netloc[:i], int(netloc[i + 1:])
+	# 	else:
+	# 		address = netloc, 80
+	return method, full_path, host#, version, scm, address, path, params, query, fragment
 
 
 def get_rawheader_met(conn):
@@ -37,8 +48,7 @@ def get_rawheader_met(conn):
 		if len(buf) < 2048:
 			break
 
-	method, version, scm, address, path, params, query, fragment =\
-		parse_header(headers)
+	method, _, _ = parse_header(headers)#, version, scm, address, path, params, query, fragment =\
 	return headers, method
 
 
@@ -58,10 +68,10 @@ def get_headers(raw_headers):
 s = requests.session()
 # context.load_cert_chain(certfile="cacert.pem", keyfile="CA.crt")
 commonname_p = re.compile('Host: (.+?)\r')
-def get_resp_for_ssl(connect, raw_headers):
+def get_req_for_ssl(connect, raw_headers):
 	# while 1:
-	host = commonname_p.findall(raw_headers)[0]
-	print(host)
+	host = commonname_p.findall(raw_headers)[0].replace(':443','')
+	# print(host)
 	# context.load_cert_chain(certfile=".zhihu.com.pem")#, keyfile="go.key")
 	# file = CertUtil.get_cert(host)
 	# print(file)
@@ -73,112 +83,85 @@ def get_resp_for_ssl(connect, raw_headers):
 	buf = 1
 	while buf:
 		buf = conn.recv(1024*8)
-		print(buf)
+		# print(buf)
 		req += buf
 		if not req.startswith(b'POST') and b'\r\n\r\n' in req\
 		   or req.startswith(b'POST') and 1< len(buf) < 2048\
 		   and not buf.endswith(b'\r\n\r\n'):
+			break
 		# if 1< len(buf) < 2048:
 		# if not buf:
-			req = get_headers(req.decode('utf-8'))
-			print(req)
-			r = s.post('http://127.0.0.1:8080',
-			# r = s.post('https://mc-bieber.rhcloud.com/',
-					   data={'req':req,'ssl':1})
-			conn.sendall(r.content)
-			break
-	conn.shutdown(socket.SHUT_RDWR)
-	conn.close()
+	try:
+		req = get_headers(req.decode('utf-8'))
+	except UnicodeDecodeError:
+		# break
+		return None, conn
+	else:
+		# if 'apilive' in req:
+		if 'websocket' in req:
+			# print(req)
+			return None, conn
+		else:
+			return req, conn
+	# print(req)
+	# conn.sendall(r.content)
+	# break
+	# conn.close()
 
 
-def handle_connection(conn):
-	# 从socket读取头
-	# print(conn)
+def handle_connection(conn, addr):
+	# while 1:
+		# conn, addr = queue.get()
 	raw_headers, method = get_rawheader_met(conn)
-	# print(repr(req_headers))
+	if raw_headers:
+		pass
+	else:
+		conn.close()
+		# continue
+		return
 	if method == 'CONNECT':
 		conn.sendall(b'HTTP/1.1 200 Connection established\r\n\r\n')
-		print('发给浏览器连接确认')
-		get_resp_for_ssl(conn, raw_headers)
+		req, conn = get_req_for_ssl(conn, raw_headers)
+		# r = s.post('http://127.0.0.1:8080',
+		# r = s.post('https://mc-bieber.rhcloud.com/',
+		if req:
+			try:
+				r = s.post('http://bjgong.tk:8080',
+						   data={'req':req,'ssl':1})
+			except requests.exceptions.ConnectionError:
+				conn.close()
+				return
+		else:
+			# conn.shutdown(socket.SHUT_RDWR)
+			conn.close()
+			# continue
+			return
+			# r = None
 
 	else:
-		headers = get_headers(raw_headers)
-		r = s.post('http://127.0.0.1:8080', data={'req':headers})
+		req = get_headers(raw_headers)
+		# r = s.post('http://127.0.0.1:8080', data={'req':headers})
+		try:
+			r = s.post('http://bjgong.tk:8080', data={'req':req})
+		except requests.exceptions.ConnectionError:
+			conn.close()
+			return
+
 		# r = s.post('https://mc-bieber.rhcloud.com', data={'req':headers})
 		# print(r.content)
 		# r = s.post('http://127.0.0.1:8080', data={'req':req})
 		# return r.content
-		conn.sendall(r.content)
-		conn.close()
-	exit()
-
-
-	
-	# print(address, req_headers)
-	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# soc.settimeout(1)
-	# 尝试连接
-	try:
-		# print('connect {}'.format(address))
-		soc.connect(address)
-		# print('connect scuess')
-	except socket.error:
-		# conn.sendall("HTTP/1.1" + str(arg[0]) + " Fail\r\n\r\n")
-		conn.close()
-		soc.close()
-	else:  # 若连接成功
-		def serv(conn, soc):
-			while 1:
-				data = conn.recv(1024*8)
-				print('cli:', len(data))
-				if not data:
-					break
-				soc.sendall(data)
-		def cli(conn, soc):
-			while 1:
-				data = soc.recv(1024*8)
-				print('serv:', len(data))
-				if not data:
-					break
-				conn.sendall(data)
-				
-		if method == 'CONNECT':
-			conn.sendall(b'HTTP/1.1 200 Connection established\r\n\r\n')
-			# try:
-			Process(target=serv, args=(conn, soc,)).start()
-			Process(target=cli, args=(conn, soc,)).start()
-			# while 1:
-				# data = conn.recv(99999)
-				# if not data:
-				# 	break
-				# soc.sendall(data)
-				# data  = soc.recv(99999)
-				# conn.sendall(data)
-			# except:
-			# 	conn.close()
-			# 	soc.close()
-					
-		else:
-			soc.sendall(req_headers.encode('utf-8'))
-			# 发送完毕, 接下来从soc读取服务器的回复
-			# 建立个缓冲区
-			# data = b''
-			# while 1:
-			# try:
-					# buf = soc.recv(1024*8)
-					# if not buf:
-					# 	break
-					# conn.sendall(buf)
-			cli(conn, soc)
-			# except socket.error:
-				# break
-				# pass
-				# data += buf
-			# 转发给客户端
-			print(address, 'ok data')
-			
-		soc.close()
-		conn.close()
+	# if r:
+	conn.sendall(r.content)
+	# else:
+	# 	pass
+	method, full_path, host = parse_header(req)
+	print( addr[0],
+		   '[{}]'.format(time.strftime('%Y-%m-%d %H:%M:%S')),
+		   '"{} {}{}"'.format(method, host, full_path) )#,
+		   # '({})'.format(i) )
+	# conn.shutdown(socket.SHUT_RDWR)
+	conn.close()
 
 
 def server():
@@ -187,16 +170,26 @@ def server():
 	s.bind(('0.0.0.0', 8087))
 	s.listen(1500)
 	print("Serving at 0.0.0.0:8087")
+	# queue_list = [ queue.Queue() for i in range(6) ]
+	# for i in range(6):
+	# 	t = Thread(target=handle_connection,args=(queue_list[i],
+	# 											  i,))
+	# 	t.setDaemon(True)
+	# 	t.start()
+
 	while 1:
+		# for q in queue_list:
 		try:
 			conn, addr = s.accept()
+			# q.put((conn,addr))
 			# print(addr)
-			Thread(target=handle_connection,args=(conn,)).start()
+			Thread(target=handle_connection,args=(conn,addr,)).start()
 			# handle_connection(conn)
 		except KeyboardInterrupt:
 			s.close()
 			print("Bye...")
-			break
+			# break
+			return
 
 if __name__ == '__main__':
 	server()
